f58f9f7f1d347fa17239c59d47c9344f3510e4c9
W2.Wizard <63303990+W2Wizard@users.noreply.github.com>
2022-08-30 14:09:02 +0200

    Create basic login page

--- frontend/src/components/button/Button.tsx
+++ frontend/src/components/button/Button.tsx
@@ -1,1 +1,28 @@
+import "./Button.scss"
 import React from 'react';
+
+////////////////////////////////////////////////////////////////////////////////
+
+export interface Properties {
+    callback: Function;
+    children?: React.ReactNode;
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+/**
+ * A button with a text value, a callback and possibly an icon.
+ */
+ const Button: React.FC<Properties> = ({ callback, children }) => {
+    const handleClick = () => {
+        callback();
+    };
+
+    return (
+        <button className="btn" onClick={handleClick}>
+            {children}
+        </button>
+    );
+};
+
+export default Button;
--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,1 +1,23 @@
 import React from "react";
+import logo from "../assets/PongLogo.gif";
+import logo42 from "../assets/42-logo.svg";
+import Button from "../components/button/Button";
+import Logger from "../utils/Logger";
+import "./Login.scss";
+
+// Login page
+const Login = () => {
+	return (
+		<div className="Login">
+			<header className="Login-header">
+				<img src={logo} className="Login-logo" alt="logo" />
+				<p>Welcome to BreadPong</p>
+				<Button callback={() => { Logger.info("Hello World!"); }}>
+					<p>LOGIN</p>
+				</Button>
+			</header>
+		</div>
+	);
+};
+
+export default Login;
