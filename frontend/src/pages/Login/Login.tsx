ded3b5e7cd04aefe0566ba16ae37f3fb8736a727
W2.Wizard <63303990+W2Wizard@users.noreply.github.com>
2022-09-19 17:15:56 +0200

    Improve navbar styling

--- frontend/src/pages/Login/Login.tsx
+++ frontend/src/pages/Login/Login.tsx
@@ -6,17 +6,18 @@
 /*   By: lde-la-h <lde-la-h@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/09/05 19:11:25 by lde-la-h      #+#    #+#                 */
-/*   Updated: 2022/09/19 13:34:36 by lde-la-h      ########   odam.nl         */
+/*   Updated: 2022/09/19 15:54:17 by lde-la-h      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 import "./Login.css"
-import React from 'react';
+import React, { useEffect } from 'react';
 import Button from "../../components/Button";
 import Logger from "../../utils/Logger";
 import LogoGIF from "../../assets/Logo.gif"
 import Logo42 from "../../assets/42Logo.png"
 import Navbar from "../../containers/Navbar";
+import { Navigate, useNavigate } from "react-router-dom";
 
 ////////////////////////////////////////////////////////////////////////////////
 
@@ -24,9 +25,11 @@ import Navbar from "../../containers/Navbar";
  * A button with a text value, a callback and possibly an icon.
  */
  const LoginPage = () => {
+
+	const navigate = useNavigate();
+
 	return (
 		<>
-
 			<div id="login">
 
 				{/* Logo */}
@@ -38,7 +41,7 @@ import Navbar from "../../containers/Navbar";
 					<p>Welcome to BreadPong</p>
 
 					{/* TODO: Connect to backend */}
-					<Button callback={() => { Logger.info("Hello World!"); }}>
+					<Button callback={() => { navigate("/home") }}>
 						<div className="login-button-content">
 							<img src={Logo42} width={28} height={28} alt="42" />
 							<p>LOGIN</p>
