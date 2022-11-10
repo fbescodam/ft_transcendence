e526d43e8a97bce6d95ddb480a9d070ee7dcc728
TheBriar <pjotrvandijk@hotmail.nl>
2022-11-10 21:52:27 +0100

    afds

--- backend/auth/intra.guard.ts
+++ backend/auth/intra.guard.ts
@@ -3,30 +3,51 @@ const jwt = require('jsonwebtoken');
 import { PrismaService } from "prisma/prisma.service";
 import { Observable } from 'rxjs';
 
+
+
 @Injectable()
 export class IntraGuard implements CanActivate {
+  constructor() {}
 
-  constructor(private prismaService: PrismaService) {
-  }
-
-  canActivate(context: any): boolean | any | Promise<boolean | any> | Observable<boolean | any> {
+  canActivate(context: any): boolean | Promise<boolean> | Observable<boolean> {
+    
     const bearerToken = context.args[0].handshake.headers.authorization.split(' ')[1];
     try {
       const decoded = jwt.verify(bearerToken, process.env.JWT_SECRET) as any;
-      return new Promise((resolve, reject) => {
-        return this.prismaService.user.findFirst({
-			where: {intraName: decoded.username}}).then(user => {
-          if (user) {
-            resolve(user);
-          } else {
-            reject(false);
-          }
-        });
+      return true;
 
-      });
-    } catch (ex) {
-      console.log(ex);
+    }
+    catch(e) {
       return false;
     }
   }
 }
+
+// @Injectable()
+// export class IntraGuard implements CanActivate {
+
+//   constructor(private prismaService: PrismaService) {
+//   }
+
+//   canActivate(context: any): boolean | any | Promise<boolean | any> | Observable<boolean | any> {
+//     const bearerToken = context.args[0].handshake.headers.authorization.split(' ')[1];
+//     try {
+//       const decoded = jwt.verify(bearerToken, process.env.JWT_SECRET) as any;
+//       return new Promise((resolve, reject) => {
+//         this.prismaService.user.findFirst({ where: 
+//           {intraName: decoded.username}}).then(user => 
+//           {
+//             if (user) {
+//               resolve(user);
+//             } else {
+//               reject(false);
+//             }
+//           });
+
+//       });
+//     } catch (ex) {
+//       console.log(ex);
+//       return false;
+//     }
+//   }
+// }
