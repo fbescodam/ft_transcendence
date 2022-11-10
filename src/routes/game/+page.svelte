1032aee076b928f1ca967760f5d5e94ad6b65541
W2.Wizard <63303990+W2Wizard@users.noreply.github.com>
2022-11-10 22:52:40 +0100

    Add matchmaking page

--- src/routes/game/+page.svelte
+++ src/routes/game/+page.svelte
@@ -3,7 +3,7 @@
 <script lang="ts">
 import Container from "$lib/Components/Container/Container.svelte";
 import TileItem from "$lib/Components/TileItem/TileItem.svelte";
-import { Globe, User, Users } from "svelte-hero-icons";
+import { Modes } from "$lib/Modes";
 
 
 </script>
@@ -15,9 +15,9 @@ import { Globe, User, Users } from "svelte-hero-icons";
 </Container>
 <hr />
 <Container style="display: flex; flex-wrap: wrap; gap: 20px; justify-content: center">
-    <TileItem href="/game" icon={User} text="Singleplayer"/>
-    <TileItem href="/game" icon={Users} text="Local Multiplayer"/>
-    <TileItem href="/game" icon={Globe} text="Online Multiplayer"/>
+    {#each Modes as mode}
+        <TileItem href="/game/matchmake?mode={mode.id}" icon={mode.icon} text="{mode.name}"/>
+    {/each}
 </Container>
 
 <!-- Styling -->
