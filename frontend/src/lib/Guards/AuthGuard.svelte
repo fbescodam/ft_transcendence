b34435a449547dadfe7199c037680cb3b3102eb0
Freek Bes <36384333+FreekBes@users.noreply.github.com>
2022-11-18 18:38:22 +0100

    disable AuthGuard for auth page

--- frontend/src/lib/Guards/AuthGuard.svelte
+++ frontend/src/lib/Guards/AuthGuard.svelte
@@ -1,8 +1,9 @@
 <script lang="ts">
 import { JWT } from "$lib/Stores/User";
 import { goto } from "$app/navigation";
+import { page } from '$app/stores';
 
-if ($JWT == null) {
+if (($JWT == null || $JWT == undefined) && !$page.url.pathname.startsWith("/auth")) {
 	goto("/auth", { replaceState: true });
 }
 
