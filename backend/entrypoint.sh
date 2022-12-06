5ee0b7c586c8f16685d26be4a5f266cadd26aed5
TheBriar <pjotrvandijk@hotmail.nl>
2022-12-06 23:56:27 +0100

    i think it works?

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
-npx prisma migrate deploy
+# while ! echo exit | nc ft_transcendence.database 3000; do sleep 5; done
+# TODO: make sure this is not needed ^
+
+npx prisma migrate dev --name penis
 npx ts-node --compiler-options {\"module\":\"CommonJS\"} prisma/seed.ts
 npm run start
