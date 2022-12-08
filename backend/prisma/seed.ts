22bb0e795744f40e62a02a7a61edcc964cd6a22a
Freek Bes <freek.bes@codam.nl>
2022-12-08 18:30:00 +0100

    fix seed errors

--- backend/prisma/seed.ts
+++ backend/prisma/seed.ts
@@ -2,21 +2,25 @@ import { PrismaClient, Role } from '@prisma/client'
 const prisma = new PrismaClient()
 
 async function main() {
-
-  await prisma.user.create({data: {
-    name: "AdminUser",
-    intraId: 80962,
-    intraName: "clafoutis",
-    avatar: "avatars/adminavatar",
-    role: Role.ADMIN,
-    email: "totally an email",
-    channels: { create: [
-      {
-        role: Role.ADMIN,
-        channel: {create: {name: "Global"}}
-      }
-  ]}}});
-
+  try {
+    await prisma.user.create({data: {
+      name: "AdminUser",
+      intraId: 80962,
+      intraName: "clafoutis",
+      avatar: "avatars/adminavatar",
+      role: Role.ADMIN,
+      email: "totally an email",
+      channels: { create: [
+        {
+          role: Role.ADMIN,
+          channel: {create: {name: "Global"}}
+        }
+    ]}}});
+  }
+  catch (e) {
+    console.log("Exception caught in seeding, the DB is probably already seeded");
+    console.trace(e);
+  }
 }
 
 //npx ts-node --compiler-options {\"module\":\"CommonJS\"} seed.ts
